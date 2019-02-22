4e9414d21ef52ffe2c469b57784c65bf540865db
Ian M. Schmutte <ischmutte@gmail.com>
2019-02-22 18:17:17 -0500

    trigger build hopefully

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::html_document2')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
