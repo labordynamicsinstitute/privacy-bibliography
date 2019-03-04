df17b47af44ca79f8542e44991e979181f103490
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-03-04 11:53:35 -0500

    Adding force option #12

--- _deploy.sh
+++ _deploy.sh
@@ -8,9 +8,12 @@ set -e
 git config --global user.email "schmutte@uga.edu"
 git config --global user.name "Ian Schmutte"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+#git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+mkdir book-output
 cd book-output
+git init
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
-git push -q origin gh-pages
+git push --force --quiet "https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}" master:gh-pages 
+#git push -q origin gh-pages
