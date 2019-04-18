09ee46f46f6bd8bc4a54a9f80eafbc482c04dba1
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-04-18 10:24:33 -0400

    Re #19 adding script to create primary.bib

---
+++ create_primary.R
@@ -0,0 +1,21 @@
+# Create primary bibliography from library.bib
+#
+# Conditionally install packages
+pkgTest <- function(x)
+{
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x,dep=TRUE)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+  return("OK")
+}
+
+global.libraries <- c("RefManageR")
+
+results <- sapply(as.list(global.libraries), pkgTest)
+
+library(RefManageR)
+library.bib <- RefManageR::ReadBib("library.bib")
+primary.bib <- library.bib[keywords="primary"]
+RefManageR::WriteBib(primary.bib,file="primary.bib")
